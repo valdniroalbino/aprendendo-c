c1b39365e2c56251e69b0666494c215306c29cb4
Valdniro Albino <Leopoldinoalbino12@hotmail.com>
2025-10-08 16:42:57 +0100

    exe001

---
+++ exe001/exe001.c
@@ -0,0 +1,10 @@
+//criar um programa que apresente "Olá, Valdniro! Seja bem-vindo à Linguagem C!".
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Olá, Valdniro! Seja bem-vindo à linguagem C!");
+
+    return 0;
+}
