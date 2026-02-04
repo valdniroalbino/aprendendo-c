9ee11ccf29ad11fda5029052a7bfaa4beaa0ead9
Jorge <Leopoldinoalbino12@hotmail.com>
2026-02-04 14:07:02 +0100

    trabalho final

---
+++ exame_T3_G2/main.c
@@ -0,0 +1,207 @@
+#include "sistema.h"
+
+
+
+void menuEleitores(Eleitor **lista){
+    int opcao;
+    do{
+        printf("\n========== GESTAO DE ELEITORES ==========\n");
+        printf("1 - Registar eleitor\n");
+        printf("2 - Atualizar eleitor\n");
+        printf("3 - Listar eleitores\n");
+        printf("4 - Carregar eleitores de ficheiro\n");
+        printf("0 - Voltar\n");
+        printf("Opcao: ");
+        scanf("%d",&opcao);
+
+        if(opcao==1){
+            int id;
+            char nome[100];
+            printf("ID do eleitor: ");
+            scanf("%d",&id);
+            printf("Nome do eleitor: ");
+            scanf("%99s",nome);
+
+            if(idExiste(*lista,id)){
+                printf("Eleitor ja existe.\n");
+            }else{
+                inserirEleitor(lista,criarEleitor(id,nome));
+                printf("Eleitor registado com sucesso.\n");
+            }
+        }
+        else if(opcao==2){
+            int id;
+            char nome[100];
+            printf("ID do eleitor: ");
+            scanf("%d",&id);
+            printf("Novo nome: ");
+            scanf("%99s",nome);
+            atualizarEleitor(*lista,id,nome);
+            printf("Eleitor atualizado.\n");
+        }
+        else if(opcao==3){
+            imprimirEleitores(*lista);
+        }
+        else if(opcao==4){
+            char ficheiro[100];
+            printf("Nome do ficheiro: ");
+            scanf("%99s",ficheiro);
+            carregarEleitoresFicheiro(lista,ficheiro);
+        }
+
+    }while(opcao!=0);
+}
+
+
+void menuCandidatos(Candidato **lista){
+    int opcao;
+    do{
+        printf("\n========== GESTAO DE CANDIDATOS ==========\n");
+        printf("1 - Registar candidato\n");
+        printf("2 - Remover candidato\n");
+        printf("3 - Listar candidatos\n");
+        printf("4 - Carregar candidatos de ficheiro\n");
+        printf("0 - Voltar\n");
+        printf("Opcao: ");
+        scanf("%d",&opcao);
+
+        if(opcao==1){
+            int id;
+            char nome[50];
+            printf("ID do candidato: ");
+            scanf("%d",&id);
+            printf("Nome do candidato: ");
+            scanf("%49s",nome);
+            inserirCandidato(lista,id,nome);
+        }
+        else if(opcao==2){
+            int id;
+            printf("ID do candidato: ");
+            scanf("%d",&id);
+            removerCandidato(lista,id);
+        }
+        else if(opcao==3){
+            imprimirCandidatos(*lista);
+        }
+        else if(opcao==4){
+            char ficheiro[100];
+            printf("Nome do ficheiro: ");
+            scanf("%99s",ficheiro);
+            carregarCandidatosFicheiro(lista,ficheiro);
+        }
+
+    }while(opcao!=0);
+}
+
+int main(){
+    Eleitor *listaEleitores=NULL;
+    Candidato *listaCandidatos=NULL;
+    Fila filas[NUM_URNAS];
+    Pilha auditoria;
+    Resultado resultado;
+
+    for(int i=0;i<NUM_URNAS;i++){
+        iniciarFila(&filas[i]);
+    }
+
+    criarPilha(&auditoria);
+    iniciarVotacao(&resultado);
+
+    int opcao;
+
+    do{
+        printf("\n=========================================\n");
+        printf("        SISTEMA DE VOTACAO ELETRONICA     \n");
+        printf("=========================================\n");
+        printf("1 - Gerir eleitores\n");
+        printf("2 - Gerir candidatos\n");
+        printf("3 - Inicializar votacao\n");
+        printf("4 - Votar\n");
+        printf("5 - Resultados parciais\n");
+        printf("6 - Resultado final\n");
+        printf("0 - Sair\n");
+        printf("Opcao: ");
+        scanf("%d",&opcao);
+
+        if(opcao==1){
+            menuEleitores(&listaEleitores);
+        }
+        else if(opcao==2){
+            menuCandidatos(&listaCandidatos);
+        }
+        else if(opcao==3){
+            Eleitor *aux=listaEleitores;
+            int indice=0;
+
+            for(int i=0;i<NUM_URNAS;i++){
+                iniciarFila(&filas[i]);
+            }
+
+            while(aux){
+                if(!aux->votou){
+                    inserirFila(&filas[indice%NUM_URNAS],aux->id,aux->nome);
+                    indice++;
+                }
+                aux=aux->prox;
+            }
+            printf("Votacao inicializada com sucesso.\n");
+        }
+        else if(opcao==4){
+            int urna,idCandidato;
+            printf("Urna (0 a %d): ",NUM_URNAS-1);
+            scanf("%d",&urna);
+
+            if(urna<0 || urna>=NUM_URNAS){
+                printf("Urna invalida.\n");
+            }
+            else if(!filas[urna].ini){
+                printf("Fila vazia.\n");
+            }
+            else{
+                printf("ID do candidato (0 branco / -1 nulo): ");
+                scanf("%d",&idCandidato);
+
+                Voto voto={1,1,2026,10,0,urna,idCandidato};
+                processarVoto(&resultado,&auditoria,voto);
+
+                if(idCandidato>0){
+                    Candidato *c=procurarCandidato(listaCandidatos,idCandidato);
+                    if(c)c->votos++;
+                }
+
+                Eleitor *e=procurarEleitor(listaEleitores,filas[urna].ini->idEle);
+                jaVotou(e);
+                removerFila(&filas[urna]);
+
+                printf("Voto registado com sucesso.\n");
+            }
+        }
+        else if(opcao==5){
+            mostrarResultadosParciais(&resultado);
+        }
+        else if(opcao==6){
+            mostrarResultadoFinal(&resultado,listaCandidatos);
+
+            int total=0;
+            Eleitor *aux=listaEleitores;
+            while(aux){
+                total++;
+                aux=aux->prox;
+            }
+
+            gerarFicheiroFinal(&resultado,listaCandidatos,total);
+            printf("Resultado final gravado em ficheiro.\n");
+        }
+
+    }while(opcao!=0);
+
+    libertarEleitores(&listaEleitores);
+    libertarCandidatos(&listaCandidatos);
+    for(int i=0;i<NUM_URNAS;i++){
+        libertarFila(&filas[i]);
+    }
+    libertarPilha(&auditoria);
+
+    printf("A sair do sistema...\n");
+    return 0;
+}
---
+++ exame_T3_G2/sistema.h
@@ -0,0 +1,104 @@
+#ifndef SISTEMA_H
+#define SISTEMA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_URNAS 3
+
+/* ================= ELEITORES ================= */
+
+typedef struct Eleitor{
+    int id;
+    char nome[100];
+    int votou;
+    struct Eleitor *prox;
+}Eleitor;
+
+Eleitor *criarEleitor(int id,const char *nome);
+void inserirEleitor(Eleitor **lista,Eleitor *novo);
+Eleitor *procurarEleitor(Eleitor *lista,int id);
+int idExiste(Eleitor *lista,int id);
+void atualizarEleitor(Eleitor *lista,int id,const char *novoNome);
+void jaVotou(Eleitor *e);
+void imprimirEleitores(Eleitor *lista);
+void carregarEleitoresFicheiro(Eleitor **lista,const char *nomeFicheiro);
+void libertarEleitores(Eleitor **lista);
+
+/* ================= CANDIDATOS ================= */
+
+typedef struct Candidato{
+    int id;
+    char nome[50];
+    int votos;
+    struct Candidato *prox;
+}Candidato;
+
+int inserirCandidato(Candidato **lista,int id,char nome[]);
+int removerCandidato(Candidato **lista,int id);
+Candidato *procurarCandidato(Candidato *lista,int id);
+void imprimirCandidatos(Candidato *lista);
+void carregarCandidatosFicheiro(Candidato **lista,const char *nomeFicheiro);
+void libertarCandidatos(Candidato **lista);
+
+/* ================= FILAS ================= */
+
+typedef struct NoFila{
+    int idEle;
+    char nome[50];
+    struct NoFila *prox;
+}NoFila;
+
+typedef struct{
+    NoFila *ini;
+    NoFila *fim;
+    int tam;
+}Fila;
+
+void iniciarFila(Fila *fila);
+int inserirFila(Fila *fila,int id,char nome[]);
+int removerFila(Fila *fila);
+int desistirFila(Fila *fila,int id);
+void imprimirFila(Fila *fila);
+int tamanhoFila(Fila *fila);
+void libertarFila(Fila *fila);
+
+/* ================= VOTO / PILHA ================= */
+
+typedef struct{
+    int dia,mes,ano;
+    int hora,minuto;
+    int urna;
+    int idCandidato;
+}Voto;
+
+typedef struct NoPilha{
+    Voto voto;
+    struct NoPilha *prox;
+}NoPilha;
+
+typedef struct{
+    NoPilha *topo;
+}Pilha;
+
+void criarPilha(Pilha *pilha);
+int pilhaVazia(Pilha *pilha);
+void empilhar(Pilha *pilha,Voto voto);
+Voto desempilhar(Pilha *pilha);
+void libertarPilha(Pilha *pilha);
+
+/* ================= RESULTADOS ================= */
+
+typedef struct{
+    int validos;
+    int brancos;
+    int nulos;
+}Resultado;
+
+void iniciarVotacao(Resultado *resultado);
+void processarVoto(Resultado *resultado,Pilha *auditoria,Voto voto);
+void mostrarResultadosParciais(Resultado *resultado);
+void mostrarResultadoFinal(Resultado *resultado,Candidato *candidatos);
+void gerarFicheiroFinal(Resultado *resultado,Candidato *candidatos,int totalEleitores);
+
+#endif
