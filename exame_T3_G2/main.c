0e4a512e37358245aa0efba2c9612011f552740d
Jorge <Leopoldinoalbino12@hotmail.com>
2026-02-07 08:44:41 +0100

    exame 3.0

--- exame_T3_G2/main.c
+++ exame_T3_G2/main.c
@@ -1,5 +1,11 @@
 #include "sistema.h"
 
+/*
+    António Nangikola - 20240694
+    Josemar da Silva - 20240610
+    Valdniro Albino - 20241757
+*/
+
 void menuEleitores(Eleitor **lista){
     int opcao;
     do{
@@ -10,46 +16,50 @@ void menuEleitores(Eleitor **lista){
         printf("4 - Carregar eleitores de ficheiro\n");
         printf("0 - Voltar\n");
         printf("Opcao: ");
-        scanf("%d",&opcao);
+        scanf("%d", &opcao);
 
-        if(opcao==1){
+        if(opcao == 1){
             int id;
             char nome[100];
-            printf("ID do eleitor: ");
-            scanf("%d",&id);
-            printf("Nome do eleitor: ");
-            scanf("%99s",nome);
 
-            if(idExiste(*lista,id)){
+            printf("ID do eleitor: ");
+            scanf("%d", &id);
+            printf("Nome do eleitor: ");
+            scanf("%99s", nome);
+
+            if(idExiste(*lista, id)){
                 printf("Eleitor ja existe.\n");
             }else{
-                inserirEleitor(lista,criarEleitor(id,nome));
+                inserirEleitor(lista, criarEleitor(id, nome));
                 printf("Eleitor registado com sucesso.\n");
             }
         }
-        else if(opcao==2){
+        else if(opcao == 2){
             int id;
             char nome[100];
+
             printf("ID do eleitor: ");
-            scanf("%d",&id);
+            scanf("%d", &id);
             printf("Novo nome: ");
-            scanf("%99s",nome);
-            atualizarEleitor(*lista,id,nome);
+            scanf("%99s", nome);
+
+            atualizarEleitor(*lista, id, nome);
             printf("Eleitor atualizado.\n");
         }
-        else if(opcao==3){
+        else if(opcao == 3){
             imprimirEleitores(*lista);
         }
-        else if(opcao==4){
+        else if(opcao == 4){
             char ficheiro[100];
+
             printf("Nome do ficheiro: ");
-            scanf("%99s",ficheiro);
-            carregarEleitoresFicheiro(lista,ficheiro);
+            scanf("%99s", ficheiro);
+
+            carregarEleitoresFicheiro(lista, ficheiro);
         }
 
-    }while(opcao!=0);
+    }while(opcao != 0);
 }
-
 
 void menuCandidatos(Candidato **lista){
     int opcao;
@@ -61,44 +71,50 @@ void menuCandidatos(Candidato **lista){
         printf("4 - Carregar candidatos de ficheiro\n");
         printf("0 - Voltar\n");
         printf("Opcao: ");
-        scanf("%d",&opcao);
+        scanf("%d", &opcao);
 
-        if(opcao==1){
+        if(opcao == 1){
             int id;
             char nome[50];
+
             printf("ID do candidato: ");
-            scanf("%d",&id);
+            scanf("%d", &id);
             printf("Nome do candidato: ");
-            scanf("%49s",nome);
-            inserirCandidato(lista,id,nome);
+            scanf("%49s", nome);
+
+            inserirCandidato(lista, id, nome);
         }
-        else if(opcao==2){
+        else if(opcao == 2){
             int id;
+
             printf("ID do candidato: ");
-            scanf("%d",&id);
-            removerCandidato(lista,id);
+            scanf("%d", &id);
+
+            removerCandidato(lista, id);
         }
-        else if(opcao==3){
+        else if(opcao == 3){
             imprimirCandidatos(*lista);
         }
-        else if(opcao==4){
+        else if(opcao == 4){
             char ficheiro[100];
+
             printf("Nome do ficheiro: ");
-            scanf("%99s",ficheiro);
-            carregarCandidatosFicheiro(lista,ficheiro);
+            scanf("%99s", ficheiro);
+
+            carregarCandidatosFicheiro(lista, ficheiro);
         }
 
-    }while(opcao!=0);
+    }while(opcao != 0);
 }
 
 int main(){
-    Eleitor *listaEleitores=NULL;
-    Candidato *listaCandidatos=NULL;
+    Eleitor *listaEleitores = NULL;
+    Candidato *listaCandidatos = NULL;
     Fila filas[NUM_URNAS];
     Pilha auditoria;
     Resultado resultado;
 
-    for(int i=0;i<NUM_URNAS;i++){
+    for(int i = 0; i < NUM_URNAS; i++){
         iniciarFila(&filas[i]);
     }
 
@@ -119,37 +135,39 @@ int main(){
         printf("6 - Resultado final\n");
         printf("0 - Sair\n");
         printf("Opcao: ");
-        scanf("%d",&opcao);
+        scanf("%d", &opcao);
 
-        if(opcao==1){
+        if(opcao == 1){
             menuEleitores(&listaEleitores);
         }
-        else if(opcao==2){
+        else if(opcao == 2){
             menuCandidatos(&listaCandidatos);
         }
-        else if(opcao==3){
-            Eleitor *aux=listaEleitores;
-            int indice=0;
+        else if(opcao == 3){
+            Eleitor *aux = listaEleitores;
+            int indice = 0;
 
-            for(int i=0;i<NUM_URNAS;i++){
+            for(int i = 0; i < NUM_URNAS; i++){
                 iniciarFila(&filas[i]);
             }
 
             while(aux){
                 if(!aux->votou){
-                    inserirFila(&filas[indice%NUM_URNAS],aux->id,aux->nome);
+                    inserirFila(&filas[indice % NUM_URNAS], aux->id, aux->nome);
                     indice++;
                 }
-                aux=aux->prox;
+                aux = aux->prox;
             }
+
             printf("Votacao inicializada com sucesso.\n");
         }
-        else if(opcao==4){
-            int urna,idCandidato;
-            printf("Urna (0 a %d): ",NUM_URNAS-1);
-            scanf("%d",&urna);
+        else if(opcao == 4){
+            int urna, idCandidato;
 
-            if(urna<0 || urna>=NUM_URNAS){
+            printf("Urna (0 a %d): ", NUM_URNAS - 1);
+            scanf("%d", &urna);
+
+            if(urna < 0 || urna >= NUM_URNAS){
                 printf("Urna invalida.\n");
             }
             else if(!filas[urna].ini){
@@ -157,47 +175,50 @@ int main(){
             }
             else{
                 printf("ID do candidato (0 branco / -1 nulo): ");
-                scanf("%d",&idCandidato);
+                scanf("%d", &idCandidato);
 
-                Voto voto={1,1,2026,10,0,urna,idCandidato};
-                processarVoto(&resultado,&auditoria,voto);
+                Voto voto = {1, 1, 2026, 10, 0, urna, idCandidato};
+                processarVoto(&resultado, &auditoria, voto);
 
-                if(idCandidato>0){
-                    Candidato *c=procurarCandidato(listaCandidatos,idCandidato);
-                    if(c)c->votos++;
+                if(idCandidato > 0){
+                    Candidato *c = procurarCandidato(listaCandidatos, idCandidato);
+                    if(c) c->votos++;
                 }
 
-                Eleitor *e=procurarEleitor(listaEleitores,filas[urna].ini->idEle);
+                Eleitor *e = procurarEleitor(listaEleitores, filas[urna].ini->idEle);
                 jaVotou(e);
                 removerFila(&filas[urna]);
 
                 printf("Voto registado com sucesso.\n");
             }
         }
-        else if(opcao==5){
+        else if(opcao == 5){
             mostrarResultadosParciais(&resultado);
         }
-        else if(opcao==6){
-            mostrarResultadoFinal(&resultado,listaCandidatos);
+        else if(opcao == 6){
+            mostrarResultadoFinal(&resultado, listaCandidatos);
 
-            int total=0;
-            Eleitor *aux=listaEleitores;
+            int total = 0;
+            Eleitor *aux = listaEleitores;
+
             while(aux){
                 total++;
-                aux=aux->prox;
+                aux = aux->prox;
             }
 
-            gerarFicheiroFinal(&resultado,listaCandidatos,total);
+            gerarFicheiroFinal(&resultado, listaCandidatos, total);
             printf("Resultado final gravado em ficheiro.\n");
         }
 
-    }while(opcao!=0);
+    }while(opcao != 0);
 
     libertarEleitores(&listaEleitores);
     libertarCandidatos(&listaCandidatos);
-    for(int i=0;i<NUM_URNAS;i++){
+
+    for(int i = 0; i < NUM_URNAS; i++){
         libertarFila(&filas[i]);
     }
+
     libertarPilha(&auditoria);
 
     printf("A sair do sistema...\n");
--- exame_T3_G2/sistema.c
+++ exame_T3_G2/sistema.c
@@ -6,20 +6,24 @@
 Eleitor *criarEleitor(int id,const char *nome){
     Eleitor *novo=malloc(sizeof(Eleitor));
     if(!novo)return NULL;
+
     novo->id=id;
     strncpy(novo->nome,nome,99);
     novo->nome[99]='\0';
     novo->votou=0;
     novo->prox=NULL;
+
     return novo;
 }
 
 void inserirEleitor(Eleitor **lista,Eleitor *novo){
     if(!novo)return;
+
     if(!*lista){
         *lista=novo;
         return;
     }
+
     Eleitor *aux=*lista;
     while(aux->prox)aux=aux->prox;
     aux->prox=novo;
@@ -27,14 +31,14 @@ void inserirEleitor(Eleitor **lista,Eleitor *novo){
 
 Eleitor *procurarEleitor(Eleitor *lista,int id){
     while(lista){
-        if(lista->id==id)return lista;
+        if(lista->id == id)return lista;
         lista=lista->prox;
     }
     return NULL;
 }
 
 int idExiste(Eleitor *lista,int id){
-    return procurarEleitor(lista,id)!=NULL;
+    return procurarEleitor(lista,id) != NULL;
 }
 
 void atualizarEleitor(Eleitor *lista,int id,const char *novoNome){
@@ -52,25 +56,29 @@ void jaVotou(Eleitor *e){
 void imprimirEleitores(Eleitor *lista){
     while(lista){
         printf("ID:%d | Nome:%s | Votou:%s\n",
-               lista->id,lista->nome,lista->votou?"Sim":"Nao");
+               lista->id,lista->nome,lista->votou ? "Sim" : "Nao");
         lista=lista->prox;
     }
 }
 
 void carregarEleitoresFicheiro(Eleitor **lista,const char *nomeFicheiro){
     FILE *f=fopen(nomeFicheiro,"r");
-    if(!f){printf("Erro ao abrir ficheiro.\n");return;}
+    if(!f){
+        printf("Erro ao abrir ficheiro.\n");
+        return;
+    }
 
     int id,votou;
     char nome[100];
 
-    while(fscanf(f,"%99s %d %d",nome,&id,&votou)==3){
+    while(fscanf(f,"%99s %d %d",nome,&id,&votou) == 3){
         Eleitor *e=criarEleitor(id,nome);
         if(e){
             e->votou=votou;
             inserirEleitor(lista,e);
         }
     }
+
     fclose(f);
 }
 
@@ -87,20 +95,23 @@ void libertarEleitores(Eleitor **lista){
 
 int inserirCandidato(Candidato **lista,int id,char nome[]){
     if(procurarCandidato(*lista,id))return 0;
+
     Candidato *c=malloc(sizeof(Candidato));
     if(!c)return 0;
+
     c->id=id;
     strncpy(c->nome,nome,49);
     c->nome[49]='\0';
     c->votos=0;
     c->prox=*lista;
     *lista=c;
+
     return 1;
 }
 
 Candidato *procurarCandidato(Candidato *lista,int id){
     while(lista){
-        if(lista->id==id)return lista;
+        if(lista->id == id)return lista;
         lista=lista->prox;
     }
     return NULL;
@@ -108,13 +119,17 @@ Candidato *procurarCandidato(Candidato *lista,int id){
 
 int removerCandidato(Candidato **lista,int id){
     Candidato *ant=NULL,*atual=*lista;
-    while(atual&&atual->id!=id){
+
+    while(atual && atual->id != id){
         ant=atual;
         atual=atual->prox;
     }
+
     if(!atual)return 0;
+
     if(!ant)*lista=atual->prox;
     else ant->prox=atual->prox;
+
     free(atual);
     return 1;
 }
@@ -129,13 +144,18 @@ void imprimirCandidatos(Candidato *lista){
 
 void carregarCandidatosFicheiro(Candidato **lista,const char *nomeFicheiro){
     FILE *f=fopen(nomeFicheiro,"r");
-    if(!f){printf("Erro ao abrir ficheiro.\n");return;}
+    if(!f){
+        printf("Erro ao abrir ficheiro.\n");
+        return;
+    }
 
     int id;
     char nome[50];
-    while(fscanf(f,"%d %49s",&id,nome)==2){
+
+    while(fscanf(f,"%d %49s",&id,nome) == 2){
         inserirCandidato(lista,id,nome);
     }
+
     fclose(f);
 }
 
@@ -159,6 +179,7 @@ void iniciarFila(Fila *fila){
 int inserirFila(Fila *fila,int id,char nome[]){
     NoFila *n=malloc(sizeof(NoFila));
     if(!n)return 0;
+
     n->idEle=id;
     strncpy(n->nome,nome,49);
     n->nome[49]='\0';
@@ -174,9 +195,12 @@ int inserirFila(Fila *fila,int id,char nome[]){
 
 int removerFila(Fila *fila){
     if(!fila->ini)return 0;
+
     NoFila *aux=fila->ini;
     fila->ini=aux->prox;
+
     if(!fila->ini)fila->fim=NULL;
+
     free(aux);
     fila->tam--;
     return 1;
@@ -184,14 +208,19 @@ int removerFila(Fila *fila){
 
 int desistirFila(Fila *fila,int id){
     NoFila *ant=NULL,*at=fila->ini;
-    while(at&&at->idEle!=id){
+
+    while(at && at->idEle != id){
         ant=at;
         at=at->prox;
     }
+
     if(!at)return 0;
+
     if(!ant)fila->ini=at->prox;
     else ant->prox=at->prox;
-    if(at==fila->fim)fila->fim=ant;
+
+    if(at == fila->fim)fila->fim=ant;
+
     free(at);
     fila->tam--;
     return 1;
@@ -205,7 +234,9 @@ void imprimirFila(Fila *fila){
     }
 }
 
-int tamanhoFila(Fila *fila){return fila->tam;}
+int tamanhoFila(Fila *fila){
+    return fila->tam;
+}
 
 void libertarFila(Fila *fila){
     while(fila->ini)removerFila(fila);
@@ -213,12 +244,18 @@ void libertarFila(Fila *fila){
 
 /* ================= PILHA ================= */
 
-void criarPilha(Pilha *pilha){pilha->topo=NULL;}
-int pilhaVazia(Pilha *pilha){return pilha->topo==NULL;}
+void criarPilha(Pilha *pilha){
+    pilha->topo=NULL;
+}
+
+int pilhaVazia(Pilha *pilha){
+    return pilha->topo == NULL;
+}
 
 void empilhar(Pilha *pilha,Voto voto){
     NoPilha *n=malloc(sizeof(NoPilha));
     if(!n)return;
+
     n->voto=voto;
     n->prox=pilha->topo;
     pilha->topo=n;
@@ -227,10 +264,12 @@ void empilhar(Pilha *pilha,Voto voto){
 Voto desempilhar(Pilha *pilha){
     Voto v={0};
     if(pilhaVazia(pilha))return v;
+
     NoPilha *aux=pilha->topo;
     v=aux->voto;
     pilha->topo=aux->prox;
     free(aux);
+
     return v;
 }
 
@@ -248,13 +287,15 @@ void iniciarVotacao(Resultado *r){
 
 void processarVoto(Resultado *r,Pilha *auditoria,Voto voto){
     empilhar(auditoria,voto);
-    if(voto.idCandidato>0)r->validos++;
-    else if(voto.idCandidato==0)r->brancos++;
+
+    if(voto.idCandidato > 0)r->validos++;
+    else if(voto.idCandidato == 0)r->brancos++;
     else r->nulos++;
 }
 
 void mostrarResultadosParciais(Resultado *r){
-    printf("Validos:%d Brancos:%d Nulos:%d\n",r->validos,r->brancos,r->nulos);
+    printf("Validos:%d Brancos:%d Nulos:%d\n",
+           r->validos,r->brancos,r->nulos);
 }
 
 void mostrarResultadoFinal(Resultado *r,Candidato *c){
@@ -267,16 +308,16 @@ void mostrarResultadoFinal(Resultado *r,Candidato *c){
     int empate=0;
 
     while(aux){
-        if(aux->votos>vencedor->votos){
+        if(aux->votos > vencedor->votos){
             vencedor=aux;
             empate=0;
-        }else if(aux!=vencedor && aux->votos==vencedor->votos){
+        }else if(aux != vencedor && aux->votos == vencedor->votos){
             empate=1;
         }
         aux=aux->prox;
     }
 
-    if(vencedor->votos==0)
+    if(vencedor->votos == 0)
         printf("Sem vencedor.\n");
     else if(empate)
         printf("Empate com %d votos.\n",vencedor->votos);
@@ -295,12 +336,12 @@ void gerarFicheiroFinal(Resultado *r,Candidato *c,int total){
     Candidato *v=c,*vencedor=NULL;
     while(v){
         fprintf(f,"%s (%d): %d votos\n",v->nome,v->id,v->votos);
-        if(!vencedor||v->votos>vencedor->votos)
+        if(!vencedor || v->votos > vencedor->votos)
             vencedor=v;
         v=v->prox;
     }
 
-    if(vencedor && vencedor->votos>0)
+    if(vencedor && vencedor->votos > 0)
         fprintf(f,"VENCEDOR: %s (%d)\n",vencedor->nome,vencedor->id);
 
     fclose(f);
